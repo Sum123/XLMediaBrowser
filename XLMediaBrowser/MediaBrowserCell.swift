76b4d371a5962a7e3305cc60d600cd12d4dece64
xialei <18612254512@163.com>
2020-08-30 23:29:17 +0800

    first commit

--- XLMediaBrowser/MediaBrowserCell.swift
+++ XLMediaBrowser/MediaBrowserCell.swift
@@ -147,7 +147,7 @@ class MediaBrowserCell: UICollectionViewCell {
         v.minimumZoomScale = minScale
         v.showsVerticalScrollIndicator = false
         v.showsHorizontalScrollIndicator = false
-        v.decelerationRate = .fast
+//        v.decelerationRate = .fast
         v.delegate = self
         v.isMultipleTouchEnabled = true
         v.scrollsToTop = false
